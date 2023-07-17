91faafa4366f38f070746ee3e387f4a93e710bd4
msosauy <131314374+msosauy@users.noreply.github.com>
2023-07-16 22:46:04 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+      brand: 'Mikrotik',
+      model: 'RB-951UI',
+      category: 'Networkin',
+      price: 100
+    },
+    {
+      brand: 'Mikrotik',
+      model: 'RB-951G',
+      category: 'Networkin',
+      price: 120
+    },
+    {
+      brand: 'Mikrotik',
+      model: 'RB-3011',
+      category: 'Networkin',
+      price: 300
+    },
+    {
+      brand: 'Ubiquiti',
+      model: 'UAP-AC-Lite',
+      category: 'Networkin',
+      price: 200
+    },
+    {
+      brand: 'Ubiquiti',
+      model: 'LiteBeam-AC',
+      category: 'Networkin',
+      price: 300
+    },
+  ]
+
+  ///false
+  
+  export default products;
