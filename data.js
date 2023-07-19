595c5d23fe3b4d901f70268e55c91ecc288e8b6b
msosa <uy.msosa@gmail.com>
2023-07-19 14:40:56 -0300

    entrga finalizada

--- data.js
+++ data.js
@@ -1,34 +1,55 @@
 const products = [
     {
+      id: 1,
       brand: 'Mikrotik',
       model: 'RB-951UI',
-      category: 'Networkin',
+      category: 'Redes',
       price: 100
     },
     {
+      id: 2,
       brand: 'Mikrotik',
       model: 'RB-951G',
       category: 'Networkin',
       price: 120
     },
     {
+      id: 3,
+      brand: 'Mikrotik',
+      model: 'RB-952-AC',
+      category: 'Redes',
+      price: 150
+    },
+    {
+      id: 4,
       brand: 'Mikrotik',
       model: 'RB-3011',
       category: 'Networkin',
       price: 300
     },
     {
+      id: 5,
       brand: 'Ubiquiti',
       model: 'UAP-AC-Lite',
       category: 'Networkin',
       price: 200
     },
     {
+      id: 6,
       brand: 'Ubiquiti',
       model: 'LiteBeam-AC',
       category: 'Networkin',
       price: 300
     },
+    {
+      id: 7,
+      brand: 'Ubiquiti',
+      model: 'LiteBeam-AC-2gen',
+      category: 'Redes',
+      price: 300
+    },
   ]
+
+  ///false
   
   export default products;
--- js/index.js
+++ js/index.js
@@ -1,54 +1,7 @@
-import products from "../data.js";
+document.getElementById("btnLogin").addEventListener("click", () => {
+  const firstName = document.getElementById("nombre").value;
+  const secName = document.getElementById("apellido").value;
 
-//El usuario elije rl tipo de busqueda
-const tarea = parseFloat(prompt("1-Marca | 2-Modelo | 3-Precio"));
-
-switch (tarea) {
-  case 1:
-    const searchBrandRaw = prompt("Ingresa la marca a buscar");
-    const searchBrand = stringCheck(searchBrandRaw);
-    search((el) => el.brand.includes(searchBrand));
-    break;
-
-  case 2:
-    const searchModel = prompt("Ingresa el modelo a buscar");
-    search((el) => el.model.includes(searchModel));
-    break;
-
-  case 3:
-    const minPrice = parseFloat(prompt("Ingresa precio mínimo:"));
-    const maxPrice = parseFloat(prompt("Ingresa precio mínimo:"));
-    compare(minPrice, maxPrice);
-    break;
-
-  default:
-    alert("La opción seleccionada no es correcta");
-    break;
-}
-
-//Transforma el string recibido (1er caracter a MAYUSCULA y los demas a MINUSCULA)
-function stringCheck(searchBrandRaw) {
-  return (
-    searchBrandRaw.charAt(0).toUpperCase() +
-    searchBrandRaw.slice(1).toLowerCase()
-  );
-}
-
-//Recibe un string y devuelve como array todos los productos que incluyan este string 
-function search(search) {
-  const resultado = products.filter(search);
-  console.log(resultado);
-}
-
-//Recibe 2 números (Precio mínimo y precio máximo) y devuelve todos lo productos que estén en ese rango de precios
-function compare(minPrice, maxPrice) {
-  for (let i = 0; i < products.length; i++) {
-    const product = products[i].price;
-    if (product <= maxPrice && product >= minPrice) {
-      console.log(products[i]);
-    }
-    if (i === products.length) {
-      console.log("No Existen productos en ese rango de precios");
-    }
-  }
-}
+  localStorage.setItem("nombre", firstName);
+  localStorage.setItem("apellido", secName);
+});
