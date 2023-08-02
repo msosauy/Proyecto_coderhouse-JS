10cae2ded0e738e416f172c4a1065202e67361a9
msosa <uy.msosa@gmail.com>
2023-08-02 13:38:04 -0300

    proyecto final 100%

--- js/index.js
+++ js/index.js
@@ -2,8 +2,14 @@ document.getElementById("btnLogin").addEventListener("click", () => {
   const firstName = document.getElementById("nombre").value;
   const secName = document.getElementById("apellido").value;
 
-  if (firstName & secName) {
+  if (firstName.trim().length === 0) {
+    console.log("Debe ingresar un nombre!!");
+  }else {
+    if (secName.trim().length === 0) {
+      console.log("Debes ingresar un apellido");
+    }
     localStorage.setItem("nombre", firstName);
     localStorage.setItem("apellido", secName);
+    window.location.replace("../pages/searchPage.html")
   }
 });
--- js/search.js
+++ js/search.js
@@ -1,54 +1,104 @@
-//Defectos a corregir: No diferencia entre mayúsculas y minúsculas
-
 //Toma los datos de LOGIN y los muestra en pantalla
-let name = localStorage.getItem(localStorage.key(0 + 1));
-let surname = localStorage.getItem(localStorage.key(0));
+let loginName = localStorage.getItem(localStorage.key(0 + 1));
+let loginSurname = localStorage.getItem(localStorage.key(0));
 
-const form = document.getElementById("form");
-form.innerHTML = `<h2>Bienvenido ${name} ${surname} </h2>`;
+//Fecha y hora
+const DateTime = luxon.DateTime
+const dt = DateTime.now();
+const fecha = dt.toLocaleString()
 
-//Simula un botón LOGOUT, bora el localstorage y redirige al "LOGIN"
+const userId = document.getElementById("userId");
+userId.innerHTML = `<h3>${fecha}</h3><h3>USUARIO: ${loginName} ${loginSurname} </h3>`;
+
+
+//Simula un botón LOGOUT, borra el localstorage y redirige al "LOGIN"
 document.getElementById("btnLogout").addEventListener("click", () => {
   localStorage.clear();
-})
+  window.location.replace("../index.html");
+});
 
-//Variable temporal, contiene la busqueda con ar
+//Variable temporal, contiene la busqueda con array
 let searchResultRaw = [];
 
+//Busqueda General
+async function busquedaGeneral(params) {
+  const res = await fetch("../data.json");
+  const data = await res.json();
+  
+  const brand = data.filter((el) =>
+    el.brand.toLowerCase().includes(params.toLowerCase())
+  );
+  const model = data.filter((el) =>
+    el.model.toLowerCase().includes(params.toLowerCase())
+  );
+  const category = data.filter((el) =>
+    el.category.toLowerCase().includes(params.toLowerCase())
+  );
+
+  searchResultRaw.push(...brand, ...model, ...category);
+  console.log("generalSearch", searchResultRaw);
+  removeDuplicate(searchResultRaw);
+  searchResultRaw = [];
+}
+
 //Busqueda por MARCA
-function busquedaMarca(params) {
-  searchResultRaw.push(products.filter((el) => el.brand.includes(params)));
+async function busquedaMarca(params) {
+  const res = await fetch("../data.json");
+  const data = await res.json();
+
+  searchResultRaw.push(
+    ...data.filter((el) =>
+      el.brand.toLowerCase().includes(params.toLowerCase())
+    )
+  );
   console.log("marca", searchResultRaw);
   removeDuplicate(searchResultRaw);
   searchResultRaw = [];
 }
 
 //Busqueda por MODELO
-function busquedaModelo(params) {
-  searchResultRaw.push(products.filter((el) => el.model.includes(params)));
+async function busquedaModelo(params) {
+  const res = await fetch("../data.json");
+  const data = await res.json();
+
+  searchResultRaw.push(
+    ...data.filter((el) =>
+      el.model.toLowerCase().includes(params.toLowerCase())
+    )
+  );
   console.log("modelo", searchResultRaw);
   removeDuplicate(searchResultRaw);
   searchResultRaw = [];
 }
 
 //Busqueda por CATEGORÍA
-function busquedaCategoria(params) {
-  searchResultRaw.push(products.filter((el) => el.category.includes(params)));
+async function busquedaCategoria(params) {
+  const res = await fetch("../data.json");
+  const data = await res.json();
+
+  searchResultRaw.push(
+    ...data.filter((el) =>
+      el.category.toLowerCase().includes(params.toLowerCase())
+    )
+  );
   console.log("categoria", searchResultRaw);
   removeDuplicate(searchResultRaw);
   searchResultRaw = [];
 }
 
 // //Recibe 2 números (Precio mínimo y precio máximo) y devuelve todos lo productos que estén en ese rango de precios
-function compare(minPrice, maxPrice) {
+async function compare(minPrice, maxPrice) {
+  const res = await fetch("../data.json");
+  const data = await res.json();
+
   let compareResultRaw = [];
 
-  for (let i = 0; i < products.length; i++) {
-    const product = products[i].price;
-    if (product <= maxPrice && product >= minPrice) {
-      compareResultRaw.push(products[i]);
+  for (let i = 0; i < data.length; i++) {
+    const productPrice = data[i].price;
+    if (productPrice <= maxPrice && productPrice >= minPrice) {
+      compareResultRaw.push(data[i]);
     }
-    if (i === products.length) {
+    if (i === data.length) {
       console.log("No Existen productos en ese rango de precios");
     }
   }
@@ -56,31 +106,28 @@ function compare(minPrice, maxPrice) {
 }
 
 //Remueve los articulos duplicados del array antes de enviarlos al HTML
-function removeDuplicate(searchResultRaw, param) {
-  let len = searchResultRaw[0].length;
+function removeDuplicate(searchResultRaw) {
+  let len = searchResultRaw.length;
   let searchResult = [];
 
   for (let i = 0; i < len; i++) {
-    const isTrue = searchResult.some(
-      (el) => el.id === searchResultRaw[0][i].id
-    );
+    const isTrue = searchResult.some((el) => el.id === searchResultRaw[i].id);
     if (!isTrue) {
-      searchResult.push(searchResultRaw[0][i]);
+      searchResult.push(searchResultRaw[i]);
     }
   }
   //Envía los articulos sin duplicados
-  console.log("62", searchResult);
   screenShow(searchResult);
 }
 
 //Imprime la lista
 function screenShow(searchResult) {
   let renderZone = document.querySelector("tbody");
-  //Bora los hijos anteriores antes de mostrar los lnuevos
+  //Bora los hijos anteriores antes de mostrar los nuevos
   while (renderZone.firstElementChild) {
     renderZone.firstElementChild.remove();
   }
-  //Itera sobre los resultados y los agreca a la Tabla
+  //Itera sobre los resultados y los agrega a la Tabla
   for (let i = 0; i < searchResult.length; i++) {
     const el = searchResult[i];
 
@@ -90,6 +137,11 @@ function screenShow(searchResult) {
     tr.innerHTML = `<td>${el.brand}</td><td>${el.model}</td><td>${el.category}</td><td>${el.price}</td>`;
   }
 }
+
+//EVENTO de busqueda General
+document.getElementById("search").addEventListener("keyup", () => {
+  busquedaGeneral(document.getElementById("search").value);
+});
 
 //EVENTO de Busqueda por MARCA
 document.getElementById("brandSearch").addEventListener("keyup", () => {
@@ -112,11 +164,3 @@ document.getElementById("maxPrice").addEventListener("keyup", () => {
   let maxPrice = document.getElementById("maxPrice").value;
   compare(minPrice, maxPrice);
 });
-
-// //Transforma el string recibido (1er caracter a MAYUSCULA y los demas a MINUSCULA)
-// function stringCheck(searchBrandRaw) {
-//   return (
-//     searchBrandRaw.charAt(0).toUpperCase() +
-//     searchBrandRaw.slice(1).toLowerCase()
-//   );
-// }
